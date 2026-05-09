fc90023d08c68a6719d8cce8184e4bfe6cb570ef
Михаил Огнев <ognev@mm-ya.ru>
2026-05-09 13:48:31 +0300

    текст комментария

--- fit_life.py
+++ fit_life.py
@@ -4,6 +4,10 @@ import sys
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
+WATER_PER_KG = 30  # Стандартная рекомендация.
+ML_PER_L = 1000  # Количество мл. в л..
+FLINE = ('-') * 40
+
 print(
     'Добро пожаловать в FitLife! ',
     'Я Ваш персональный помощник на пути к здоровой жизни.',
@@ -12,14 +16,31 @@ user_name = input(
     'Давайте знакомиться, '
     'мое имя Фил, а Ваше?'
 )
-print('Приятно познакомиться,', user_name + '!')
-user_age = int(input('Подскажите Ваш возраст?'))
-# Следующие два значения преобразуем в тип "float".
-user_weight = float(input('Ваш вес (в кг)?'))
-user_height = float(input(
-    'Ваш рост (введите рост '
-    'в метрах, используя точку: 1.87)?')
-)
+print(f'Приятно познакомиться, {user_name}!')
+
+while True:
+    try:
+        user_age = int(input('Подскажите Ваш возраст?'))
+        break
+    except ValueError:
+        print('Попробуйте еще раз, введите число.')
+
+while True:
+    try:
+        user_weight = float(input('Ваш вес (в кг)?'))
+        break
+    except ValueError:
+        print('Попробуйте еще раз, введите число.')
+
+while True:
+    try:
+        user_height = float(input(
+            'Ваш рост (введите рост в метрах, '
+            'используя точку: 1.87)? '))
+        break
+    except ValueError:
+        print('Пожалуйста, введите корректное число.')
+
 print()
 print(
     'Отлично! Первым делом, узнаем Ваш ИМТ ',
@@ -27,14 +48,13 @@ print(
 )
 bmi = user_weight / (user_height ** 2)
 # Рассчитаываем индекс массы тела.
-WATER_PER_KG = 30  # Стандартная рекомендация.
-ML_PER_L = 1000  # Количество мл. в л..
+
 water_nedded_ml = user_weight * WATER_PER_KG
 # Затем норму воды в день.
 water_nedded_l = water_nedded_ml / ML_PER_L  # Переводим в литры.
 # ИМТ и норму воды ограничиваем одним знаком после запятой,
 # для удобства пользователя.
-print('-' * 40)
+print(FLINE)
 print(
     f'Отчет для пользоватателя: {user_name} ({user_age} г.)\n',
     f'Ваш вес: {user_weight} кг.\n',
@@ -42,5 +62,5 @@ print(
     f'Ваш ИМТ: {round(bmi, 1)}\n',
     f'Рекомендуемая норма воды: {round(water_nedded_l, 1)} л. в день',
 )
-print('-' * 40)
+print(FLINE)
 print("Расчет окончен. Будьте здоровы!")
